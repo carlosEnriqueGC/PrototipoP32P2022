4c1ed90348031f04ca955b98e7a7da5bc35e127a
carlos enrique guzman cabrera <cguzmanc10@miumg.edu.gt>
2022-04-22 14:40:07 -0600

    se creo script

--- PrototipoP32P2022/scripts/scriptGeneral.sql
+++ PrototipoP32P2022/scripts/scriptGeneral.sql
@@ -1,73 +1,102 @@
-CREATE SCHEMA IF NOT EXISTS `bdproyectop312022` DEFAULT CHARACTER SET utf8 ;
-USE `bdproyectop312022` ;
+CREATE SCHEMA IF NOT EXISTS `prototipoP32P2022` DEFAULT CHARACTER SET utf8 ;
+USE `prototipoP32P2022` ;
 
-CREATE TABLE IF NOT EXISTS tbl_usuario (
+CREATE TABLE IF NOT EXISTS tbl_usuarios (
 	usuid INT NOT NULL AUTO_INCREMENT,
 	usunombre VARCHAR(45) NOT NULL,
-	usucontrasena VARCHAR(25) NOT NULL,
-	usuultimasesion DATE,
-	usuestatus VARCHAR(1) NOT NULL,
-	usunombrereal VARCHAR(60),
-	usucorreoe VARCHAR(60),
-	usutelefono VARCHAR(25),
-	usudireccion VARCHAR(80),
+    usucontrasena VARCHAR(45) NOT NULL,
+    Tipousu VARCHAR(45) NOT NULL,
 	PRIMARY KEY (usuid) )
 ENGINE = InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_perfil (
-    perid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    pernombre VARCHAR(30) NOT NULL,
-    perestatus VARCHAR (30) NOT NULL
-)ENGINE=InnoDB CHARACTER SET = latin1;
-
-CREATE TABLE IF NOT EXISTS tbl_aplicacion (
-	aplid int NOT NULL AUTO_INCREMENT,
-	aplnombre VARCHAR(50),
-	aplestatus VARCHAR(50),
-	PRIMARY KEY (aplid)
-) ENGINE=InnoDB CHARACTER SET = latin1;    
-
-CREATE TABLE IF NOT EXISTS tbl_modulo (
-	modid INT NOT NULL AUTO_INCREMENT,
-	modnombre VARCHAR(60) NOT NULL,
-	modestatus VARCHAR(60) NOT NULL,
-	PRIMARY KEY (modid) )
+CREATE TABLE IF NOT EXISTS tbl_tipousuarios (
+	tipousuid INT NOT NULL AUTO_INCREMENT,
+	tipousunombre VARCHAR(45) NOT NULL,
+	PRIMARY KEY (tipousuid) )
 ENGINE = InnoDB CHARACTER SET = latin1;
 
-    
- CREATE TABLE IF NOT EXISTS tbl_aplicacionPerfil(
-	aplid INT NOT NULL,
-    perid INT NOT NULL,
-    PRIMARY KEY(aplid, perid),
-    FOREIGN KEY (aplid) REFERENCES tbl_aplicacion (aplid),     
-    FOREIGN KEY (perid) REFERENCES tbl_perfil (perid)
- ) ENGINE=InnoDB CHARACTER SET = latin1;     
+CREATE TABLE IF NOT EXISTS tbl_empleados (
+	emplid INT NOT NULL AUTO_INCREMENT,
+	emplnombre VARCHAR(45) NOT NULL,
+    emplapellidos VARCHAR(45) NOT NULL,
+    empldireccion VARCHAR(45) NOT NULL,
+    emplTipousu VARCHAR(45) NOT NULL,
+    emplnacimiento DATE NOT NULL,
+    empldpi int (11) NOT NULL,
+	PRIMARY KEY (emplid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_aplicacionusuario (
-	aplid INT NOT NULL,
-	usuid INT NOT NULL,
-	PRIMARY KEY (aplid, usuid), 
-	FOREIGN KEY (aplid) references tbl_aplicacion (aplid),
-	FOREIGN KEY (usuid) references tbl_usuario (usuid)
-) ENGINE = InnoDB CHARACTER SET = latin1;
+CREATE TABLE IF NOT EXISTS tbl_entrenadores (
+	entreid INT NOT NULL AUTO_INCREMENT,
+	entrenombre VARCHAR(45) NOT NULL,
+    entreapellidos VARCHAR(45) NOT NULL,
+    entredireccion VARCHAR(45) NOT NULL,
+    entrenacimiento DATE NOT NULL,
+	PRIMARY KEY (entreid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_perfilusuario (
-	perid int NOT NULL,
-	usuid int NOT NULL,
-	PRIMARY KEY (perid, usuid),
-	FOREIGN KEY (perid) REFERENCES tbl_perfil (perid),
-	FOREIGN KEY (usuid) REFERENCES tbl_usuario (usuid)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS tbl_equipos (
+	equiid INT NOT NULL AUTO_INCREMENT,
+	equinombre VARCHAR(45) NOT NULL,
+    equitorneosganados VARCHAR(45) NOT NULL,
+	equitorneosperdidos VARCHAR(45) NOT NULL,
+    equientre VARCHAR(45) NOT NULL,
+    equideporte VARCHAR(45) NOT NULL,
+	PRIMARY KEY (equiid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_bitacora (
-    bitid int auto_increment PRIMARY KEY,
-    bitfecha datetime NULL, 
-	bitaccion VARCHAR(10) NOT NULL,
-    usuid INT NOT NULL,
-    aplid INT NOT NULL,
-	FOREIGN KEY (aplid) references tbl_aplicacion (aplid),
-	FOREIGN KEY (usuid) references tbl_usuario (usuid)    
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;	
+CREATE TABLE IF NOT EXISTS tbl_jugadores (
+	jugaid INT NOT NULL AUTO_INCREMENT,
+	juganombre VARCHAR(45) NOT NULL,
+    jugaapellidos VARCHAR(45) NOT NULL,
+    jugadireccion VARCHAR(45) NOT NULL,
+    juganacimiento DATE NOT NULL,
+	PRIMARY KEY (jugaid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
 
-   
+CREATE TABLE IF NOT EXISTS tbl_tiposjugadores (
+	tiposjugaid INT NOT NULL AUTO_INCREMENT,
+	tiposjuganombre VARCHAR(45) NOT NULL,
+    tiposjugadeporte VARCHAR(45) NOT NULL,
+	PRIMARY KEY (tiposjugaid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS tbl_deportes (
+	deporid INT NOT NULL AUTO_INCREMENT,
+	depornombre VARCHAR(45) NOT NULL, 
+	PRIMARY KEY (deporid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
+
+CREATE TABLE IF NOT EXISTS tbl_sedes (
+	sedeid INT NOT NULL AUTO_INCREMENT,
+	sedenombre VARCHAR(45) NOT NULL,
+    sededireccion VARCHAR(45) NOT NULL,
+    sedepais VARCHAR(45) NOT NULL,
+    sedecapacidad INT(20) NOT NULL,
+	PRIMARY KEY (sedeid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS tbl_pais (
+	paisid INT NOT NULL AUTO_INCREMENT,
+	paisnombre VARCHAR(45) NOT NULL,
+	PRIMARY KEY (paisid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
+
+CREATE TABLE IF NOT EXISTS tbl_campeonatosencurso (
+	camid INT NOT NULL AUTO_INCREMENT,
+	camnombre VARCHAR(45) NOT NULL,
+    camcantidad int(45) NOT NULL,
+	PRIMARY KEY (camid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
+CREATE TABLE IF NOT EXISTS tbl_campeonatoscerrados (
+	campid INT NOT NULL AUTO_INCREMENT,
+	camnpombre VARCHAR(45) NOT NULL,
+    campcantidad int(45) NOT NULL,
+	campganador VARCHAR(45) NOT NULL,
+	PRIMARY KEY (campid) )
+ENGINE = InnoDB CHARACTER SET = latin1;
+
 
